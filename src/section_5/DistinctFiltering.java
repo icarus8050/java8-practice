cca5bc58c024f4bf2e1d953c2d73feb69961c2bf
icarus8050 <icarus8050@naver.com>
2020-03-01 22:19:15 +0900

    DistinctFiltering Exam

---
+++ src/section_5/DistinctFiltering.java
@@ -0,0 +1,14 @@
+package section_5;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DistinctFiltering {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
+        numbers.stream()
+                .filter(i -> i % 2 == 0)
+                .distinct()
+                .forEach(System.out::println);
+    }
+}
