13214477ddf9541474a98b116bfa19d8ffb453ec
icarus8050 <icarus8050@naver.com>
2020-03-03 00:24:43 +0900

    1 ~ 2 문제 해결

--- src/section_5/actual_practice5_5/Main.java
+++ src/section_5/actual_practice5_5/Main.java
@@ -1,7 +1,9 @@
 package section_5.actual_practice5_5;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,6 +21,27 @@ public class Main {
                 new Transaction(alan, 2012, 950)
         );
 
+        System.out.println("------------------------------------------------------");
+        System.out.println("1. 2011년에 일어난 모든 트랜잭션을 찾아 값을 오름차순으로 정리하시오.");
+        transactions.stream()
+                .filter(transaction -> transaction.getYear() == 2011)
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .forEach(transaction -> System.out.println(transaction.toString()));
+        System.out.println("------------------------------------------------------");
+        System.out.println("2. 거래자가 근무하는 모든 도시를 중복 없이 나열하시오.");
+        transactions.stream()
+                .map(transaction -> transaction.getTrader().getCity())
+                .distinct()
+                .forEach(System.out::println);
 
+        System.out.println();
+        System.out.println("or");
+        System.out.println();
+
+        transactions.stream()
+                .map(transaction -> transaction.getTrader().getCity())
+                .collect(Collectors.toSet())
+                .forEach(System.out::println);
+        System.out.println("------------------------------------------------------");
     }
 }
