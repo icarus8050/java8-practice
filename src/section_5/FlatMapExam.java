76712cea3c5dce92540d5e282706559920b103c1
icarus8050 <icarus8050@naver.com>
2020-03-05 00:19:52 +0900

    FlatMap 다른 예제 연습

--- src/section_5/FlatMapExam.java
+++ src/section_5/FlatMapExam.java
@@ -2,6 +2,7 @@ package section_5;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FlatMapExam {
     public static void main(String[] args) {
@@ -13,5 +14,18 @@ public class FlatMapExam {
                 .distinct()
                 .collect(Collectors.toList())
                 .forEach(System.out::println);
+
+        System.out.println("----------------------------------------------");
+        String[][] data = new String[][]{{"a", "b"}, {"c", "d"}, {"e", "f"}};
+
+        //Stream<String[]>
+        Stream<String[]> temp = Arrays.stream(data);
+
+        //Stream<String>, GOOD!
+        Stream<String> stringStream = temp.flatMap(Arrays::stream);
+
+        Stream<String> stream = stringStream.filter(x -> "a".equals(x.toString()));
+
+        stream.forEach(System.out::println);
     }
 }
