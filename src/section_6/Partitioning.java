9a26c4a14d72bc64b82195df98d80e28c735f80f
icarus8050 <icarus8050@naver.com>
2020-03-08 20:07:15 +0900

    Two Level Mapping

--- src/section_6/Partitioning.java
+++ src/section_6/Partitioning.java
@@ -10,9 +10,19 @@ import java.util.stream.Collectors;
 public class Partitioning {
     public static void main(String[] args) {
         List<Dish> menu = ExampleFactory.getExamData();
+
         Map<Boolean, List<Dish>> simplePartition =
                 menu.stream().collect(Collectors.partitioningBy(Dish::isVegetarian));
 
         System.out.println(simplePartition);
+        System.out.println("---------------------------------------------------");
+
+        Map<Boolean, Map<Dish.Type, List<Dish>>> twoLevelMap = menu.stream().collect(
+                Collectors.partitioningBy(
+                        Dish::isVegetarian,
+                        Collectors.groupingBy(Dish::getType)
+                )
+        );
+        System.out.println(twoLevelMap);
     }
 }
