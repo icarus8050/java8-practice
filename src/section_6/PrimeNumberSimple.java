5195aeb5869f990cc78466a403efe17f8a8f05c3
icarus8050 <icarus8050@naver.com>
2020-03-11 00:47:27 +0900

    PrimeNumber Advanced custom code

--- src/section_6/PrimeNumberSimple.java
+++ src/section_6/PrimeNumberSimple.java
@@ -9,11 +9,20 @@ public class PrimeNumberSimple {
     public static void main(String[] args) {
         Map<Boolean, List<Integer>> primeNumbersPartition = partitionPrimes(11);
         System.out.println(primeNumbersPartition);
+        System.out.println("---------------------------");
+        System.out.println("Advanced PrimeNumberCollector");
+        Map<Boolean, List<Integer>> advancedPrimeNumbersPartition = advancedPartitionPrimes(11);
+        System.out.println(advancedPrimeNumbersPartition);
     }
 
     public static Map<Boolean, List<Integer>> partitionPrimes(int n) {
         return IntStream.rangeClosed(2, n).boxed()
                 .collect(Collectors.partitioningBy(PrimeNumberSimple::isPrime));
+    }
+
+    public static Map<Boolean, List<Integer>> advancedPartitionPrimes(int n) {
+        return IntStream.rangeClosed(2, n).boxed()
+                .collect(new PrimeNumberAdvanced());
     }
 
     public static boolean isPrime(int candidate) {
