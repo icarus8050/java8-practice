d6208b76db66645de66311b20b4ca538e984e5db
icarus8050 <icarus8050@naver.com>
2020-03-17 00:30:18 +0900

    단어 사이에 공백이 여러 개일 때도 동작하는지 확인

--- src/section_7/CustomSpliterator.java
+++ src/section_7/CustomSpliterator.java
@@ -1,8 +1,16 @@
 package section_7;
 
 public class CustomSpliterator {
+    public static void main(String[] args) {
+        final String SENTENCE =
+                "Nel   mezzo del cammin   di  nostra  vita " +
+                        "mi   ritrovai  in una  selva oscura" +
+                        " ch   la   dritta  via era  smarrita ";
 
-    public int countWordsIteratively(String s) {
+        System.out.println("Found " + countWordsIteratively(SENTENCE) + " words");
+    }
+
+    public static int countWordsIteratively(String s) {
         int counter = 0;
         boolean lastSpace = true;
         for (char c : s.toCharArray()) {
