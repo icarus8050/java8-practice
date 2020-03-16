0f576fd9de125dc0dd4816699caea39a661a0f55
icarus8050 <icarus8050@naver.com>
2020-03-17 01:04:55 +0900

    리듀싱을 이용한 단어 수 확인 코드

--- src/section_7/CustomSpliterator.java
+++ src/section_7/CustomSpliterator.java
@@ -1,5 +1,8 @@
 package section_7;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class CustomSpliterator {
     public static void main(String[] args) {
         final String SENTENCE =
@@ -8,6 +11,11 @@ public class CustomSpliterator {
                         " ch   la   dritta  via era  smarrita ";
 
         System.out.println("Found " + countWordsIteratively(SENTENCE) + " words");
+
+        System.out.println("----------------------------------");
+        Stream<Character> stream = IntStream.range(0, SENTENCE.length())
+                .mapToObj(SENTENCE::charAt);
+        System.out.println("Found " + countWords(stream) + " words");
     }
 
     public static int countWordsIteratively(String s) {
@@ -26,5 +34,45 @@ public class CustomSpliterator {
         }
 
         return counter;
+    }
+
+    public static int countWords(Stream<Character> stream) {
+        WordCounter wordCounter = stream.reduce(
+                new WordCounter(0, true),
+                WordCounter::accumulate,
+                WordCounter::combine);
+
+        return wordCounter.getCounter();
+    }
+
+    public static class WordCounter {
+        private final int counter;
+        private final boolean lastSpace;
+
+        public WordCounter(int counter, boolean lastSpace) {
+            this.counter = counter;
+            this.lastSpace = lastSpace;
+        }
+
+        public WordCounter accumulate(Character c) {
+            if (Character.isWhitespace(c)) {
+                return lastSpace ?
+                        this :
+                        new WordCounter(counter, true);
+            } else {
+                return lastSpace ?
+                        new WordCounter(counter + 1, false) :
+                        this;
+            }
+        }
+
+        public WordCounter combine(WordCounter wordCounter) {
+            return new WordCounter(counter + wordCounter.counter,
+                    wordCounter.lastSpace);
+        }
+
+        public int getCounter() {
+            return counter;
+        }
     }
 }
