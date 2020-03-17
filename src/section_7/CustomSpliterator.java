b7315c12ff39a4d1747f23ac4410327cafb78f04
icarus8050 <icarus8050@naver.com>
2020-03-18 00:59:20 +0900

    Spliterator 를 이용한 병렬 처리

--- src/section_7/CustomSpliterator.java
+++ src/section_7/CustomSpliterator.java
@@ -1,7 +1,10 @@
 package section_7;
 
+import java.util.Spliterator;
+import java.util.function.Consumer;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class CustomSpliterator {
     public static void main(String[] args) {
@@ -16,6 +19,10 @@ public class CustomSpliterator {
         Stream<Character> stream = IntStream.range(0, SENTENCE.length())
                 .mapToObj(SENTENCE::charAt);
         System.out.println("Found " + countWords(stream) + " words");
+        System.out.println("----------------------------------");
+        Spliterator<Character> spliterator = new WordCounterSpliterator(SENTENCE);
+        Stream<Character> characterStream = StreamSupport.stream(spliterator, true);
+        System.out.println("Found " + countWords(characterStream) + " words");
     }
 
     public static int countWordsIteratively(String s) {
@@ -73,6 +80,50 @@ public class CustomSpliterator {
 
         public int getCounter() {
             return counter;
+        }
+    }
+
+    public static class WordCounterSpliterator implements Spliterator<Character> {
+
+        private final String string;
+        private int currentChar = 0;
+
+        public WordCounterSpliterator(String string) {
+            this.string = string;
+        }
+
+        @Override
+        public boolean tryAdvance(Consumer<? super Character> action) {
+            action.accept(string.charAt(currentChar++));
+            return currentChar < string.length();
+        }
+
+        @Override
+        public Spliterator<Character> trySplit() {
+            int currentSize = string.length() - currentChar;
+            if (currentSize < 10) {
+                return null;
+            }
+            for (int splitPos = currentSize / 2 + currentChar;
+                 splitPos < string.length(); splitPos++) {
+                if (Character.isWhitespace(string.charAt(splitPos))) {
+                    Spliterator<Character> spliterator =
+                            new WordCounterSpliterator(string.substring(currentChar, splitPos));
+                    currentChar = splitPos;
+                    return spliterator;
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public long estimateSize() {
+            return string.length() - currentChar;
+        }
+
+        @Override
+        public int characteristics() {
+            return ORDERED + SIZED + SUBSIZED + NONNULL + IMMUTABLE;
         }
     }
 }
