09003573a4fd0f94ca3c9bff9b0b6f3325868349
icarus8050 <icarus8050@naver.com>
2020-03-19 00:18:40 +0900

    Strategy Pattern 에서 람다를 이용한 함수 전달 방법

--- src/section_8/strategy/Main.java
+++ src/section_8/strategy/Main.java
@@ -2,6 +2,8 @@ package section_8.strategy;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("---------------------");
+        System.out.println("일반적인 표현 방법");
         Validator numericValidator = new Validator(new IsNumeric());
         boolean b1 = numericValidator.validate("aaaa");
         System.out.println(b1);
@@ -9,5 +11,15 @@ public class Main {
         Validator lowerCaseValidator = new Validator(new IsAllLowerCase());
         boolean b2 = lowerCaseValidator.validate("bbbb");
         System.out.println(b2);
+
+        System.out.println("---------------------");
+        System.out.println("람다 표현식을 이용한 방법");
+        Validator numericValidator2 = new Validator((String s) -> s.matches("\\d+"));
+        boolean b3 = numericValidator2.validate("aaaa");
+        System.out.println(b3);
+
+        Validator lowerCaseValidator2 = new Validator((String s) -> s.matches("[a-z]+"));
+        boolean b4 = lowerCaseValidator2.validate("bbbb");
+        System.out.println(b4);
     }
 }
