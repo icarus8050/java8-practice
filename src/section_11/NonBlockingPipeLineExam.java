863d34aab05a435d2ad0f7e33d47fd2a9f6fe45e
icarus8050 <icarus8050@naver.com>
2020-03-28 00:44:57 +0900

    Thread pool async exam

--- src/section_11/NonBlockingPipeLineExam.java
+++ src/section_11/NonBlockingPipeLineExam.java
@@ -2,9 +2,24 @@ package section_11;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.stream.Collectors;
 
 public class NonBlockingPipeLineExam {
+
+    private static final Executor executor =
+            Executors.newFixedThreadPool(5, new ThreadFactory() {
+                @Override
+                public Thread newThread(Runnable r) {
+                    Thread t = new Thread(r);
+                    t.setDaemon(true);
+                    return t;
+                }
+            });
+
     public static void main(String[] args) {
         List<Shop> shops = Arrays.asList(
                 new Shop("BestPrice"),
@@ -21,10 +36,20 @@ public class NonBlockingPipeLineExam {
     }
 
     public static List<String> findPrices(List<Shop> shops) {
-        return shops.stream()
-                .map(shop -> shop.getPrice(shop.getName()))
-                .map(Quote::parse)
-                .map(Discount::applyDiscount)
+        List<CompletableFuture<String>> priceFutures =
+                shops.stream()
+                        .map(shop -> CompletableFuture.supplyAsync(
+                                () -> shop.getPrice(shop.getName()), executor
+                        ))
+                        .map(future -> future.thenApply(Quote::parse))
+                        .map(future -> future.thenCompose(
+                                quote -> CompletableFuture.supplyAsync(
+                                        () -> Discount.applyDiscount(quote), executor)
+                        ))
+                        .collect(Collectors.toList());
+
+        return priceFutures.stream()
+                .map(CompletableFuture::join)
                 .collect(Collectors.toList());
     }
 }
