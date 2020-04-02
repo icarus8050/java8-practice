c95ddbcecad6ed0dba97db4aa789783fc6254b43
icarus8050 <icarus8050@naver.com>
2020-04-03 01:51:23 +0900

    Lazy List sample

---
+++ src/section_14/lazy/LazyList.java
@@ -0,0 +1,28 @@
+package section_14.lazy;
+
+import java.util.function.Supplier;
+
+public class LazyList<T> implements MyList<T> {
+    final T head;
+    final Supplier<MyList<T>> tail;
+
+    public LazyList(T head, Supplier<MyList<T>> tail) {
+        this.head = head;
+        this.tail = tail;
+    }
+
+    @Override
+    public T head() {
+        return head;
+    }
+
+    @Override
+    public MyList<T> tail() {
+        return tail.get();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
--- src/section_14/lazy/Main.java
+++ src/section_14/lazy/Main.java
@@ -2,6 +2,17 @@ package section_14.lazy;
 
 public class Main {
     public static void main(String[] args) {
-        MyList<Integer> l = new MyLinkedList<>(5, new MyLinkedList<>(10, new Empty<>()));
+        LazyList<Integer> numbers = from(2);
+        int two = numbers.head();
+        int three = numbers.tail().head();
+        int four = numbers.tail().tail().head();
+
+        System.out.println(two);
+        System.out.println(three);
+        System.out.println(four);
+    }
+
+    public static LazyList<Integer> from(int n) {
+        return new LazyList<>(n, () -> from(n + 1));
     }
 }
