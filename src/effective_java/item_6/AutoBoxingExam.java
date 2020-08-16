ae720df119a71e91e75cbe7d85e52cbba9737516
icarus8050 <icarus8050@naver.com>
2020-08-16 15:23:38 +0900

    Item 6. 불필요한 객체 생성을 피하라

---
+++ src/effective_java/item_6/AutoBoxingExam.java
@@ -0,0 +1,11 @@
+package effective_java.item_6;
+
+public class AutoBoxingExam {
+    private static long sum() {
+        Long sum = 0L;
+        for (long i = 0; i <= Integer.MAX_VALUE; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
---
+++ src/effective_java/item_6/MapExam.java
@@ -0,0 +1,23 @@
+package effective_java.item_6;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+public class MapExam {
+    public static void main(String[] args) {
+        Map<Integer, String> maps = new HashMap<>();
+
+        maps.put(1, "one");
+        maps.put(2, "two");
+        maps.put(3, "three");
+        maps.put(4, "four");
+        maps.put(5, "five");
+        System.out.println(maps);
+
+        Set<Integer> set1 = maps.keySet();
+        Set<Integer> set2 = maps.keySet();
+        System.out.println(set1 == set2);
+    }
+}
