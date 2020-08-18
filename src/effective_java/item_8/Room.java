30807cfe6cb2bc6400541c6a26bedac7d2ffa3eb
icarus8050 <icarus8050@naver.com>
2020-08-18 21:14:34 +0900

    Item 8. finalizer와 cleaner 사용을 피하라

---
+++ src/effective_java/item_8/Room.java
@@ -0,0 +1,35 @@
+package effective_java.item_8;
+
+import java.lang.ref.Cleaner;
+
+public class Room implements AutoCloseable {
+    private static final Cleaner cleaner = Cleaner.create();
+
+    private static class State implements Runnable {
+        int numJunkPiles;
+
+        State(int numJunkPiles) {
+            this.numJunkPiles = numJunkPiles;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("방 청소");
+            numJunkPiles = 0;
+        }
+    }
+
+    private final State state;
+
+    private final Cleaner.Cleanable cleanable;
+
+    public Room(int numJunkPiles) {
+        state = new State(numJunkPiles);
+        cleanable = cleaner.register(this, state);
+    }
+
+    @Override
+    public void close() throws Exception {
+        cleanable.clean();
+    }
+}
