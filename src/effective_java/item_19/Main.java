feb8a003ab0a16ba803e5f315bfeb477c4646b02
icarus8050 <icarus8050@naver.com>
2020-08-30 22:28:20 +0900

    상속용 클래스에서 생성자가 재저으이 가능 메서드를 호출하는 경우의 문제 예시

---
+++ src/effective_java/item_19/Main.java
@@ -0,0 +1,8 @@
+package effective_java.item_19;
+
+public class Main {
+    public static void main(String[] args) {
+        Sub sub = new Sub();
+        sub.overrideMe();
+    }
+}
---
+++ src/effective_java/item_19/Super.java
@@ -0,0 +1,13 @@
+package effective_java.item_19;
+
+public class Super {
+
+    // 잘못된 예 - 생성자가 재정의 가능 메서드를 호출한다.
+    public Super() {
+        overrideMe();
+    }
+
+    public void overrideMe() {
+
+    }
+}
