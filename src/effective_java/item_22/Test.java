0d55d286b7657ba7ec9cc65a93f4b0960ec64688
icarus8050 <icarus8050@naver.com>
2020-09-01 22:14:25 +0900

    인터페이스 안티패턴 예시 (상수 인터페이스)

---
+++ src/effective_java/item_22/Test.java
@@ -0,0 +1,10 @@
+package effective_java.item_22;
+
+import static effective_java.item_22.PhysicalConstants.*;
+
+public class Test {
+
+    double atoms(double mols) {
+        return AVOGADROS_NUMBER * mols;
+    }
+}
