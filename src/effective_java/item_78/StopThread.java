fbb6d16556c3325401414569a0654c4e2b710ed1
icarus8050 <icarus8050@naver.com>
2020-11-04 00:08:55 +0900

    동기화를 통한 스레드 정상 동작 예제 코드 (volatile)

--- src/effective_java/item_78/StopThread.java
+++ src/effective_java/item_78/StopThread.java
@@ -3,7 +3,7 @@ package effective_java.item_78;
 import java.util.concurrent.TimeUnit;
 
 public class StopThread {
-    private static boolean stopRequested;
+    /*private static boolean stopRequested;
 
     private static synchronized void requestStop() {
         stopRequested = true;
@@ -24,5 +24,20 @@ public class StopThread {
 
         TimeUnit.SECONDS.sleep(1);
         requestStop();
+    }*/
+
+    private static volatile boolean stopRequested;
+
+    public static void main(String[] args)
+            throws InterruptedException {
+        Thread backgroundThread = new Thread(() -> {
+            int i = 0;
+            while (!stopRequested)
+                i++;
+        });
+        backgroundThread.start();
+
+        TimeUnit.SECONDS.sleep(1);
+        stopRequested = true;
     }
 }
