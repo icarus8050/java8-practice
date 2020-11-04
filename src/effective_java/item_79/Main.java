a9d77cfcf6aefaae1c404220b3790638f86a3ce3
icarus8050 <icarus8050@naver.com>
2020-11-05 00:18:49 +0900

    메인 스레드와 백그라운드 스레드가 교착상태를 일으킨다

--- src/effective_java/item_79/Main.java
+++ src/effective_java/item_79/Main.java
@@ -1,18 +1,42 @@
 package effective_java.item_79;
 
 import java.util.HashSet;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
         ObservableSet<Integer> set =
                 new ObservableSet<>(new HashSet<>());
 
-        set.addObserver(new SetObserver<Integer>() {
+        /*set.addObserver((s, e) -> System.out.println(e));*/
+
+        /*set.addObserver(new SetObserver<Integer>() {
             @Override
             public void added(ObservableSet<Integer> set, Integer element) {
                 System.out.println(element);
                 if (element == 23)
                     set.removeObserver(this);
+            }
+        });*/
+
+        set.addObserver(new SetObserver<Integer>() {
+            @Override
+            public void added(ObservableSet<Integer> set, Integer element) {
+                System.out.println(element);
+                if (element == 23) {
+                    ExecutorService exec =
+                            Executors.newSingleThreadExecutor();
+
+                    try {
+                        exec.submit(() -> set.removeObserver(this)).get();
+                    } catch (ExecutionException | InterruptedException ex) {
+                        throw new AssertionError(ex);
+                    } finally {
+                        exec.shutdown();
+                    }
+                }
             }
         });
 
